fbbb5719da4f15aef969a673fe5050e9ac261b2a
JABEDUL ALAM <jabedulalam681@gmail.com>
2021-10-07 12:33:19 +0600

    heder html part1,2

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,11 @@
 $(function (){
-    script
+    // top header news slider js strat
+        $('.header_news').slick({
+            arrows:false,
+            vertical:true,
+            verticalSwiping:true,
+            autoplay: true,
+            autoplaySpeed: 500,
+        })
+     // top header news slider js end
 })
